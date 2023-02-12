930c9fdc637e4c1dfc2984345846cf00aeba36d1
lixinyang123 <lixinyangemil@outlook.com>
2023-02-12 12:26:19 +0800

    添加项目文件。

---
+++ src/Program.cs
@@ -0,0 +1,42 @@
+﻿using PhotinoNET;
+using System.Drawing;
+using System.Text;
+
+namespace HelloPhotinoApp
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] _)
+        {
+            string windowTitle = "Photino for .NET Demo App";
+
+            var window = new PhotinoWindow()
+                .SetTitle(windowTitle)
+                .SetUseOsDefaultSize(false)
+                .SetSize(new Size(1000, 800))
+                .Center()
+                .SetResizable(false)
+                .RegisterCustomSchemeHandler("app", (object sender, string scheme, string url, out string contentType) =>
+                {
+                    contentType = "text/javascript";
+                    return new MemoryStream(Encoding.UTF8.GetBytes(@"
+                        (() =>{
+                            window.setTimeout(() => {
+                                alert(`🎉 Dynamically inserted JavaScript.`);
+                            }, 1000);
+                        })();
+                    "));
+                })
+                .RegisterWebMessageReceivedHandler((object? sender, string message) =>
+                {
+                    PhotinoWindow? window = sender as PhotinoWindow;
+                    string response = $"Received message: \"{message}\"";
+                    window?.SendWebMessage(response);
+                })
+                .Load("wwwroot/index.html");
+
+            window.WaitForClose();
+        }
+    }
+}
