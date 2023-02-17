f8c1f89730277973e8a20b26ea284934a16d0b1f
lixinyang123 <lixinyangemil@outlook.com>
2023-02-18 04:43:07 +0800

    add publish script

---
+++ publish.ps1
@@ -0,0 +1,4 @@
+dotnet publish -r win-x64 -c Release
+Remove-Item src\bin\Release\net7.0\win-x64\publish\PhotinoAOT.pdb
+Copy-Item src\wwwroot src\bin\Release\net7.0\win-x64\publish -Recurse -Force
+makensis installer.nsi
